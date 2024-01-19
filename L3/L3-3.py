e7db63e6c670e96eb6cf7b273b7a33555e0462da
mtdykly <2224692217@qq.com>
2024-01-19 09:15:53 +0800

    删除了L3

--- L3/L3-3.py
+++
@@ -1,8 +0,0 @@
-import re
-id_number=input("请输入身份证号：")
-m=re.match("(^\d{15}$)|(^\d{17}([0-9]|x)$)",id_number)
-if m:
-    print(m.group(0))
-else:
-    print("not match")
-    
--- L3/L3-9.py
+++
@@ -1,9 +0,0 @@
-A=input("请输入一个数组 用空格隔开:").split(' ')
-A=[int(x) for x in A]
-B=[0 for i in range(len(A))]
-for i in range (len(A)):
-    B[i]=1
-    for j in range(len(A)):
-        if i!=j:
-            B[i]=B[i]*A[j]
-print(B)
